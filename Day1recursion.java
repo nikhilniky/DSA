4a77d4deda20e641a0c5ba69043a395c553ffe57
nikhilniky <37295610+nikhilniky@users.noreply.github.com>
2022-05-19 20:15:19 +0530

    Update Day1recursion.java

--- Day1recursion.java
+++ Day1recursion.java
@@ -24,4 +24,26 @@ class Solution {
 	}
 
 
+//Sum
+
+import java.util.*;
+
+class Solution {
+	
+	public static void main(String args[]) {
+   Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+		if(n>=0)
+			System.out.println(recursive(n));
+		else
+				System.out.println("Error");
+	}
+	 static int recursive(int x)
+		{
+			if(x==1||x==0)
+			return 1;
+			else
+				return x+recursive(x-1);
+		}	
+	}
 
