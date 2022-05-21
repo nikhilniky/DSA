e4fc779d4de2b2268a940bbb702c659088096e45
nikhilniky <37295610+nikhilniky@users.noreply.github.com>
2022-05-21 20:14:36 +0530

    Create day4 recursion.java

---
+++ day4 recursion.java
@@ -0,0 +1,26 @@
+
+//https://www.codingninjas.com/codestudio/problems/check-palindrome_4219630
+//palindrome using recursion
+//two pointer approach 
+// we will start comparing first and last letter of the string
+
+
+
+public class Solution {
+    public static void main(String []args){
+        Solution ans= new Solution();
+        String obj="level";
+        ans.isPalindrome(obj);
+    }
+	public static Boolean Palindrome(int i, int j,String s) {
+		if(i>=j) return true;
+    if(s.charAt(i)!=s.charAt(j)) return false;
+    return Palindrome(i+1,j-1,s);
+	} 
+	
+    public static Boolean isPalindrome(String s) {
+		return Palindrome(0,s.length()-1,s);
+
+	}
+
+}
