c54aca53261f5b5a4687bf5ca4176036b03c93f4
nikhilniky <37295610+nikhilniky@users.noreply.github.com>
2022-06-08 21:03:06 +0530

    Update Comibnation11subset.java

--- Comibnation11subset.java
+++ Comibnation11subset.java
@@ -2,3 +2,54 @@
 
 
 https://www.codingninjas.com/codestudio/problems/combination-sum-ii_1112622
+
+
+
+import java.util.*;
+
+public class Solution {
+    public static ArrayList<ArrayList<Integer>> result;
+
+    private static void solve(ArrayList<Integer> currList, int currIndex, int currSum, int rqSum, ArrayList<Integer> arr) {
+        if (currSum == rqSum) 
+        {
+            result.add(new ArrayList<Integer>(currList));
+            return;
+        }
+        if (currIndex == arr.size()) 
+        {
+            return;
+        }
+
+        solve(currList, currIndex + 1, currSum, rqSum, arr);
+
+        int count = 0;
+
+        while (currSum <= rqSum) 
+        {
+            count++;
+
+            currSum += arr.get(currIndex);
+
+            currList.add(arr.get(currIndex));
+
+            solve(currList, currIndex + 1, currSum, rqSum, arr);
+        }
+        while (count > 0) 
+        {
+            int index = currList.size() - 1;
+            currList.remove(index);
+            count--;
+        }
+    }
+
+    public static ArrayList<ArrayList<Integer>> combSum(ArrayList<Integer> arr, int rqSum) 
+    {
+
+        Collections.sort(arr);
+        result = new ArrayList<ArrayList<Integer>>();
+        ArrayList<Integer> currList = new ArrayList<Integer>();
+        solve(currList, 0, 0, rqSum, arr);
+        return result;
+    }
+}
